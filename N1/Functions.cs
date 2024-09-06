6924cc1a1fc0646914b81c9499739c45b323a64b
pharaneeshwaran.j <pharaneeshj@gmail.com>
2024-09-06 09:49:07 +0530

    Added the functions

---
+++ N1/Functions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1
+{
+    class function
+    {
+        static void withoutarg()
+        {
+            Console.WriteLine("i just call the method");
+        }
+        static void Main(String[] args)
+        {
+            withoutarg();
+        }
+    }
+}
+
