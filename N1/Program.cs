511bb3fa96da4c3706a72994aa9616cf9bdb869f
Chanthini.K <kchanthinimurugan@gmail.com>
2024-09-06 10:21:12 +0530

    method overloading

--- N1/Program.cs
+++ N1/Program.cs
@@ -1,3 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello,!");
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Numerics;
+namespace basics;
+class G1
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("********Parameterized method***********");
+        value(1, 2);
+        Console.WriteLine("********non-Parameterized method***********");
+        simple();
+        Console.WriteLine("********Method Overloading***********");
+        Console.WriteLine(add(3, 4));
+        Console.WriteLine(add(3, 4,5));
+        Console.WriteLine(add(3, 4, 5,1));
+
+
+    }
+    public static int value(int a, int b)
+    {
+        Console.WriteLine("parameterized method");
+        Console.WriteLine(a + ", " + b);
+        return a;
+    }
+    public static void simple()
+    {
+        Console.WriteLine("No argument method");
+    }
+    //METHOD OVERLOADING
+    public static  int add(int a, int b)
+    {
+        return(a + b);
+    }
+    public static int add(int a, int b, int c)
+    {
+        return (a + b+c);
+    }
+    public static  int add(int a, int b, int c, int d)
+    {
+        return (a + b + c +d);
+    }
+
+
+
+}
+
