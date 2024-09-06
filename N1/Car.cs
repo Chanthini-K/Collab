e7de0b0b3c321e2e066bab6c629a03a51b9d1703
pharaneeshwaran.j <pharaneeshj@gmail.com>
2024-09-06 17:27:45 +0530

    constructor added

---
+++ N1/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1
+{
+    class Vehicle  
+    {
+        public string brand = "Ford";  
+     
+        public void Honk()             
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+
+    class Car : Vehicle 
+    {
+        public string modelName = "Mustang"; 
+    }
+}
--- N1/Functions.cs
+++ N1/Functions.cs
@@ -2,52 +2,57 @@
 {
     class Function
     {
-        static void Withoutarg()
-        {
-            Console.WriteLine("i just call the method");
-        }
-        static void Car(string model = "BMW") {
-            Console.WriteLine($" A car model is {model}");
-        }
-        static void Calculation(int x, int y)
-        {
-            int a = x + y;
-            Console.WriteLine($"Addition:{a}");
-            int c = x - y;
-            Console.WriteLine($"Substraction:{c}");
-            int b = x * y;
-            Console.WriteLine($"Multipilcation:{b}");
-        }
-        //-------------return-------------------
-        static int Add(int x, int y)
-        {
-            return x + y;
-        }
-        //------------------Method Overloading----------------------------
-        static string Add(string x)
-        {
-            return x + "j";
-        }
-        //----------------Different parameter----------------------
-        static int Add(int x, int y,int z)
-        {
-            return x + y + z;
-        }
-        //---------------Accessing with Key-----------------------
-        static string Vechile( string name,string color)
-        {
-            return name  +" color is " + color;
-        }
+        //static void Withoutarg()
+        //{
+        //    Console.WriteLine("i just call the method");
+        //}
+        //static void Car(string model = "BMW") {
+        //    Console.WriteLine($" A car model is {model}");
+        //}
+        //static void Calculation(int x, int y)
+        //{
+        //    int a = x + y;
+        //    Console.WriteLine($"Addition:{a}");
+        //    int c = x - y;
+        //    Console.WriteLine($"Substraction:{c}");
+        //    int b = x * y;
+        //    Console.WriteLine($"Multipilcation:{b}");
+        //}
+        ////-------------return-------------------
+        //static int Add(int x, int y)
+        //{
+        //    return x + y;
+        //}
+        ////------------------Method Overloading----------------------------
+        //static string Add(string x)
+        //{
+        //    return x + "j";
+        //}
+        ////----------------Different parameter----------------------
+        //static int Add(int x, int y,int z)
+        //{
+        //    return x + y + z;
+        //}
+        ////---------------Accessing with Key-----------------------
+        //static string Vechile( string name,string color)
+        //{
+        //    return name  +" color is " + color;
+        //}
+        //--------------------Class and Object-----------------------------
+
         static void Main(String[] args)
         {
-            Withoutarg();
-            Car();
-            Car("scoda");
-            Calculation( 5,3);
-            Console.WriteLine(Add(7, 4));
-            Console.WriteLine(Add(7, 4,6));
-            Console.WriteLine(Add("pharaneesh"));
-            Console.WriteLine(Vechile(name: "Bike", color: "Red"));
+            //Withoutarg();
+            //Car();
+            //Car("scoda");
+            //Calculation( 5,3);
+            //Console.WriteLine(Add(7, 4));
+            //Console.WriteLine(Add(7, 4,6));
+            //Console.WriteLine(Add("pharaneesh"));
+            //Console.WriteLine(Vechile(name: "Bike", color: "Red"));
+            Car obj1 = new Car();
+            obj1.Honk();
+            Console.WriteLine(obj1.brand + " " + obj1.modelName);
         }
     }
 }
