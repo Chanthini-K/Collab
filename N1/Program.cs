794ff29397a74a2536ca06db061a540cce5eaaf6
Chanthini.K <kchanthinimurugan@gmail.com>
2024-09-06 12:57:28 +0530

    Constructor completed

--- N1/Program.cs
+++ N1/Program.cs
@@ -13,8 +13,13 @@ class G1
         simple();
         Console.WriteLine("********Method Overloading***********");
         Console.WriteLine(add(3, 4));
-        Console.WriteLine(add(3, 4,5));
-        Console.WriteLine(add(3, 4, 5,1));
+        Console.WriteLine(add(3, 4, 5));
+        Console.WriteLine(add(3, 4, 5, 1));
+        Console.WriteLine("********Parameterized and No argument constructor ***********");
+        G1 avi1 = new G1();
+        G1 avi2 = new G1(43.2);
+
+
 
 
     }
@@ -29,20 +34,28 @@ class G1
         Console.WriteLine("No argument method");
     }
     //METHOD OVERLOADING
-    public static  int add(int a, int b)
+    public static int add(int a, int b)
     {
-        return(a + b);
+        return (a + b);
     }
     public static int add(int a, int b, int c)
     {
-        return (a + b+c);
+        return (a + b + c);
     }
-    public static  int add(int a, int b, int c, int d)
+    public static int add(int a, int b, int c, int d)
     {
-        return (a + b + c +d);
+        return (a + b + c + d);
     }
-
-
-
+    public G1()
+    {
+        Console.WriteLine("This is no argument constructor");
+    }
+    public G1(double d)
+    {
+        Console.WriteLine("This is Parameterized Constructor: " + d);
+    }
+    
+   
 }
+
 
